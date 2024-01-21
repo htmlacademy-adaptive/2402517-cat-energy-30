5caa5e435d77727ce1ff6cfd52fb0f0dcdf0a326
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-01-21 12:56:35 +0300

    Строит сетки features

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,30 +1,29 @@
 /* в этот файл добавляет скрипты*/
-let menu__toggle = document.querySelector("button.header__toggle");
-let menu = document.querySelector("nav.header__nav");
-let viewPort = window.innerWidth;
+const menuToggle = document.querySelector('button.header__toggle');
+const menu = document.querySelector('nav.header__nav');
+const viewPort = window.innerWidth;
 
 if (viewPort < 768) {
-  window.addEventListener("load", function defaultMenu() {
-  menu.classList.add("header__nav--has-close");
-  })
+  window.addEventListener('load', () => {
+    menu.classList.add('header__nav--has-close');
+  });
 }
 
 // не хочет работать :с -> при изменении ширины до таблетки вернуть дисплей навигации
-// window.addEventListener("resize", function checkViewPort () {
+// window.addEventListener('resize', function checkViewPort () {
 //   if (viewPort > 767) {
-//     menu.classList.remove("header__nav--has-close")
+//     menu.classList.remove('header__nav--has-close')
 //   }
 // })
 
 // if (window.innerWidth > 767) {
-//   window.addEventListener("resize", function showNav() {
-//     menu.classList.remove("header__nav--has-close");
+//   window.addEventListener('resize', function showNav() {
+//     menu.classList.remove('header__nav--has-close');
 //   })
 // }
 
-menu__toggle.addEventListener("click", function openMenu() {
-  menu__toggle.classList.toggle("header__toggle--has-open");
-  menu.classList.toggle("header__nav--has-open");
-  menu.classList.toggle("header__nav--has-close");
-})
-
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('header__toggle--has-open');
+  menu.classList.toggle('header__nav--has-open');
+  menu.classList.toggle('header__nav--has-close');
+});
