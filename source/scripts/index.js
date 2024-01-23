7b0ebbc479afa610a1126b30a51a04c06ee5bf1f
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-01-23 09:12:39 +0300

    Строит сетки example

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,10 @@
 const menuToggle = document.querySelector('button.header__toggle');
 const menu = document.querySelector('nav.header__nav');
 const viewPort = window.innerWidth;
+const sliderToggle = document.querySelector('button.slider__toggle');
+const sliderAfter = document.querySelector('div.slider__after');
+const sliderContainer = document.querySelector('div.example__slider');
+let mousePressed = false;
 
 if (viewPort < 768) {
   window.addEventListener('load', () => {
@@ -9,21 +13,29 @@ if (viewPort < 768) {
   });
 }
 
-// не хочет работать :с -> при изменении ширины до таблетки вернуть дисплей навигации
-// window.addEventListener('resize', function checkViewPort () {
-//   if (viewPort > 767) {
-//     menu.classList.remove('header__nav--has-close')
-//   }
-// })
-
-// if (window.innerWidth > 767) {
-//   window.addEventListener('resize', function showNav() {
-//     menu.classList.remove('header__nav--has-close');
-//   })
-// }
-
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('header__toggle--has-open');
   menu.classList.toggle('header__nav--has-open');
   menu.classList.toggle('header__nav--has-close');
+});
+
+sliderToggle.addEventListener('mousedown', () => {
+  // console.log('mousedown');
+  mousePressed = true;
+});
+
+document.addEventListener('mouseup', () => {
+  // console.log('mouseup');
+  mousePressed = false;
+});
+
+document.addEventListener('mousemove', (event) => {
+  if (mousePressed) {
+    // console.log('mousemove', event.clientX);
+    const rightBorder = sliderAfter.getBoundingClientRect().right;
+    const leftBorder = sliderContainer.getBoundingClientRect().left;
+    const mouseX = (event.clientX < leftBorder) ? leftBorder : event.clientX;
+    const width = rightBorder - mouseX;
+    sliderAfter.style.width = `${width}px`;
+  }
 });
